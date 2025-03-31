5a328955f8e7666dd716714e8abad67baae45e0b
Bernsue <bernadette.durante@gmail.com>
2025-03-31 20:46:42 +0800

    Clearing everything again

--- angular-course/src/app/app.component.ts
+++ angular-course/src/app/app.component.ts
@@ -3,10 +3,14 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-course';
+
+  isMorning: boolean = false;
+  isDark: boolean = false;
 }
